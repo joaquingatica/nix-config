e4910dfbe00f23f16ce5b18235ed80522b19f255
Joaquin Gatica <joaquin@anagram.dev>
2025-01-24 10:47:22 -0300

    feat: add extra builtin for decrypting file path in host

--- extra-builtins.nix
+++ extra-builtins.nix
@@ -1,4 +1,4 @@
-{exec, ...}: {
+{exec, ...}: let
   # Decrypt a file using sops and return the result as a string
   decrypt = file:
     exec [
@@ -13,4 +13,24 @@
         nix-instantiate --eval --expr "builtins.readFile $f"
       ''
     ];
+
+  # Decrypt a file using sops and return the resulting path as a string
+  decryptFile = tmpPrefix: file:
+    exec [
+      "bash"
+      "-c"
+      ''
+        set -euo pipefail
+        f="$(mktemp -p "$(getconf DARWIN_USER_TEMP_DIR)" ${tmpPrefix}.XXXXXXXXXX)"
+        set -x
+        sops -d "${file}" > $f
+        echo $f
+      ''
+    ];
+
+  # Decrypt a file using sops and return the resulting path as a string
+  decryptFilePath = tmpPrefix: file:
+    builtins.toPath (decryptFile tmpPrefix file);
+in {
+  inherit decrypt decryptFilePath;
 }
