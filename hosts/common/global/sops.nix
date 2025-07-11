2279d165e93f750e356471c7aa8f9a85417291bd
Joaquin Gatica <joaquin@anagram.dev>
2025-07-10 22:50:17 -0300

    fix: use primary user for sops secrets

--- hosts/common/global/sops.nix
+++ hosts/common/global/sops.nix
@@ -1,6 +1,6 @@
 {config, ...}: let
-  user = config.users.users.joaquin;
-  configDir = "${user.home}/.config";
+  userName = config.system.primaryUser;
+  configDir = "${config.system.primaryUserHome}/.config";
 in {
   sops = {
     age.keyFile = "${configDir}/sops/age/keys.txt";
@@ -9,17 +9,17 @@ in {
         sopsFile = ./secrets/awscli.yaml;
         key = "config";
         path = "${configDir}/aws/config";
-        owner = user.name;
+        owner = userName;
       };
       "gradle/gradle.properties" = {
         sopsFile = ./secrets/secrets.yaml;
         path = "${configDir}/gradle/gradle.properties";
-        owner = user.name;
+        owner = userName;
       };
       "npm/npmrc" = {
         sopsFile = ./secrets/secrets.yaml;
         path = "${configDir}/npm/.npmrc";
-        owner = user.name;
+        owner = userName;
       };
     };
   };
