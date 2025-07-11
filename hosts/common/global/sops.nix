5e76599b1c822f19df6baf739559623656535fb5
Joaquin Gatica <joaquin@anagram.dev>
2025-07-10 22:13:47 -0300

    fix: add sops secrets owner

--- hosts/common/global/sops.nix
+++ hosts/common/global/sops.nix
@@ -1,21 +1,25 @@
 {config, ...}: let
-  configHome = "${config.system.primaryUserHome}/.config";
+  user = config.users.users.joaquin;
+  configDir = "${user.home}/.config";
 in {
   sops = {
-    age.keyFile = "${configHome}/sops/age/keys.txt";
+    age.keyFile = "${configDir}/sops/age/keys.txt";
     secrets = {
       "awscli/config" = {
         sopsFile = ./secrets/awscli.yaml;
         key = "config";
-        path = "${configHome}/aws/config";
+        path = "${configDir}/aws/config";
+        owner = user.name;
       };
       "gradle/gradle.properties" = {
         sopsFile = ./secrets/secrets.yaml;
-        path = "${configHome}/gradle/gradle.properties";
+        path = "${configDir}/gradle/gradle.properties";
+        owner = user.name;
       };
       "npm/npmrc" = {
         sopsFile = ./secrets/secrets.yaml;
-        path = "${configHome}/npm/.npmrc";
+        path = "${configDir}/npm/.npmrc";
+        owner = user.name;
       };
     };
   };
