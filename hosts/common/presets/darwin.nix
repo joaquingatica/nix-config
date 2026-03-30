dd3057fbe0b00ed9409bfb8f6d8e7df73a6e6c96
Joaquin Gatica <joaquin@anagram.dev>
2026-03-30 13:27:04 -0300

    fix: add pinentry_mac to resolve gpg signing error

--- hosts/common/presets/darwin.nix
+++ hosts/common/presets/darwin.nix
@@ -11,6 +11,7 @@
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     unstable.colima
+    pinentry_mac # to resolve error "gpg: signing failed: No pinentry" during git commit signing
   ];
 
   homebrew = {
