0655f8b644c524c5cd76f06ffc78f571fddcfd8f
Joaquin Gatica <me@joaquingatica.com>
2026-06-04 14:35:05 -0300

    refactor: sort properties in gulp-cli package

--- home/joaquin/common/global/npm-packages/gulp-cli/default.nix
+++ home/joaquin/common/global/npm-packages/gulp-cli/default.nix
@@ -3,34 +3,31 @@
   fetchFromGitHub,
   jq,
   nodejs_22,
-}: let
+}:
+buildNpmPackage (finalAttrs: {
+  pname = "gulp-cli";
   version = "3.1.0";
-in
-  buildNpmPackage {
-    pname = "gulp-cli";
-    inherit version;
 
-    src = fetchFromGitHub {
-      owner = "gulpjs";
-      repo = "gulp-cli";
-      rev = "v${version}";
-      hash = "sha256-eLorGF/aZwzi+c6sabmgkqvryNtznt3mJVc/Vmfa9aE=";
-    };
+  src = fetchFromGitHub {
+    owner = "gulpjs";
+    repo = "gulp-cli";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-eLorGF/aZwzi+c6sabmgkqvryNtznt3mJVc/Vmfa9aE=";
+  };
 
-    nodejs = nodejs_22;
+  nodejs = nodejs_22;
+  nativeBuildInputs = [jq];
 
-    nativeBuildInputs = [jq];
+  postPatch = ''
+    cp ${./package-lock.json} package-lock.json
+    # The published tarball ships a prebuilt gulp.1 manpage that is not in
+    # the git source. Drop the man entry so npm pack does not look for it.
+    jq 'del(.man) | .files |= map(select(. != "gulp.1"))' package.json > package.json.tmp
+    mv package.json.tmp package.json
+  '';
 
-    postPatch = ''
-      cp ${./package-lock.json} package-lock.json
-      # The published tarball ships a prebuilt gulp.1 manpage that is not in
-      # the git source. Drop the man entry so npm pack does not look for it.
-      jq 'del(.man) | .files |= map(select(. != "gulp.1"))' package.json > package.json.tmp
-      mv package.json.tmp package.json
-    '';
+  npmDepsHash = "sha256-GQmVIkKYlsRXZLS4WLk47hmn87tmIu+AwShkzkK0Ih8=";
 
-    npmDepsHash = "sha256-GQmVIkKYlsRXZLS4WLk47hmn87tmIu+AwShkzkK0Ih8=";
-
-    dontNpmBuild = true;
-    dontNpmPrune = true;
-  }
+  dontNpmBuild = true;
+  dontNpmPrune = true;
+})
