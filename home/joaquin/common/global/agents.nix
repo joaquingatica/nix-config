07402459e8f6c432977ddc4f3e467130b3ba4f00
Joaquin Gatica <me@joaquingatica.com>
2026-08-18 09:14:59 -0300

    feat: add matt pockock skills to claude code

--- home/joaquin/common/global/agents.nix
+++ home/joaquin/common/global/agents.nix
@@ -8,6 +8,7 @@
         enabledPlugins = {
           "frontend-design@claude-plugins-official" = true;
           "gitkraken-hooks@gitkraken" = true;
+          "mattpocock-skills@claude-plugins-official" = true;
         };
         extraKnownMarketplaces = {
           gitkraken = {
